68f97cd8d1dcc90540fe79dee31d37544f0eaabe
Whatifya <123612174+Whatifya@users.noreply.github.com>
2023-07-12 20:23:27 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>CaptionAI</title>
+        <title>Windows Copilot</title>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💭</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>"
         />
       </Head>
 
